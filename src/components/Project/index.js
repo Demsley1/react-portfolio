58219f6e709cbd4d9854ca0809f2d37ea4b997ab
Denva <emsley3684@gmail.com>
2022-02-26 12:36:36 -0500

    added more projects to file

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,33 +3,54 @@ import React from 'react';
 const Project = () => {
     return (
         <ul className="d-flex flex-wrap justify-content-between my-5 list-group-flush">
-            <li className="m-2 list-group-item d-grid">
+            <li className="m-2 list-group-item col-5 d-grid">
                 <a href="https://still-ridge-48307.herokuapp.com/" target="_blank">
-                    <img className="img" src="/images/soundwave-wallpaper.jpg" />
+                    <img className="img" src="/images/mixer.jpg" />
                 </a>
 
                 <h5 className="text-center my-3">Musit</h5>
             </li>
 
-            <li className="m-2 list-group-item d-grid">
+            <li className="m-2 list-group-item col-5 d-grid">
                 <a href="https://immense-island-78021.herokuapp.com/" target="_blank">
-                    <img className="img" src="/images/thought-spiral.jpg" />
+                    <img className="img" src="/images/mindgasm.jpg" />
                 </a>
 
                 <h5 className="text-center my-3">Deep-Thoughts</h5>
             </li>
 
-            <li className="m-2 list-group-item">
-                <h5>Project 3</h5>
+            <li className="m-2 list-group-item col-5 d-grid">
+                <a href="https://demsley1.github.io/weather-app/" target="_blank">
+                    <img className="img" src="/images/weather-splash.jpg" />
+                </a>
 
-                <p>Descripton for Project</p>
+                <h5 className="text-center my-3">Weather-App</h5>
             </li>
 
-            <li className="m-2 list-group-item">
-                <h5>Project 4</h5>
+            <li className="m-2 list-group-item col-5 d-grid">
+                <a href="https://mariovillaquiran.github.io/teamreactive/" target="_blank">
+                    <img className="img" src="/images/airplane.jpg" />
+                </a>
 
-                <p>Descripton for Project</p>
+                <h5 className="text-center my-3">Skipt</h5>
             </li>
+
+            <li className="m-2 list-group-item col-5 d-grid">
+                <a href="https://safe-caverns-00312.herokuapp.com/" target="_blank">
+                    <img className="img" src="/images/tech-blog.jpg" />
+                </a>
+
+                <h5 className="text-center my-3">Tech-Splash</h5>
+            </li>
+
+            <li className="m-2 list-group-item col-5 d-grid">
+                <a href="https://demsley1.github.io/photo-port/" target="_blank">
+                    <img className="img" src="/images/camera-lens.jpg" />
+                </a>
+
+                <h5 className="text-center my-3">Photo-Port</h5>
+            </li>
+
         </ul>
     );
 };
