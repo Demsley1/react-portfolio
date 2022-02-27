ead06604610658b2347e4f2770238f6848ffc485
Denva <emsley3684@gmail.com>
2022-02-27 00:08:50 -0500

    added link to resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 function Resume() {
     return (
-        <div>
-            Here is my Resume
+        <div className="d-flex flex-column justify-content-center align-content-center align-items-center">   
+            <h1 className="text-center my-4">Resume/Skills</h1>
 
+            <div className="d-flex">
+                <p className="resume-link">
+                    <span className="">Click this link to download my Resume:     </span>
+                    <a id="Resume" href="/links/Resume-starter.pub" download>Resume</a>
+                </p>
+            </div>
+            
+            <div>
+                <h2>Proficiencies</h2>
+
+                <ol>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>SQL</li>
+                    <li>MongoDB</li>
+                    <li>ORM</li>
+                </ol>
+            </div>
+            
         </div>
     )
 };
